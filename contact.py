a16ce095da2aa2afa2b46ea84cf8a70af432f7c0
Lena <nikolskaya_elena@mail.ru>
2017-04-19 16:56:09 +0300

    Задание №3
    Сделать всё то же самое, но для контактов

---
+++ contact.py
@@ -0,0 +1,29 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax,
+                 email, email2, email3, homepage, day1, month1, byear, day2, month2, ayear,
+                 address2, phone2, notes):
+        self.firstname=firstname
+        self.middlename=middlename
+        self.lastname=lastname
+        self.nickname = nickname
+        self.title=title
+        self.company=company
+        self.address=address
+        self.home=home
+        self.mobile=mobile
+        self.work=work
+        self.fax=fax
+        self.email=email
+        self.email2=email2
+        self.email3=email3
+        self.homepage=homepage
+        self.day1=day1
+        self.month1=month1
+        self.byear=byear
+        self.day2=day2
+        self.month2=month2
+        self.ayear=ayear
+        self.address2=address2
+        self.phone2=phone2
+        self.notes=notes
+
