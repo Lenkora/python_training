a7006962ad87dc874c6dc0eb49feef76b05dde99
Lena <nikolskaya_elena@mail.ru>
2017-04-24 17:48:16 +0300

    Задание №4
    Перевести тесты с unittest на py.test и реализовать фикстуры

--- test_add_group.py
+++ test_add_group.py
@@ -4,13 +4,13 @@ from group import Group
 from application import Application
 
 
-
 @pytest.fixture
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-    
+
+
 def test_add_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="test", header="rrr", footer="ttt"))
