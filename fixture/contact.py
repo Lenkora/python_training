51bfb1da7478aadb45498b667dbd8585b697d29a
Lena <nikolskaya_elena@mail.ru>
2017-04-26 15:25:19 +0300

    Задание №7: Сделать все тесты создания, удаления, модификации для групп и контактов
    Реализовать все недостающие (не реализованные в предыдущих домашних
    заданиях) тесты и набор вспомогательных методов, какие потребуются, для
    создания, удаления и модификации групп и контактов.
    
    При этом вспомогательные методы сразу добавлять в соответствующие
    классы-помощники. Для создания вспомогательных методов можно
    использовать рекордер, но сценарий целиком не записывать, и тесты делать
    вручную -- как последовательность обращений к вспомогательным методам.
    
    Возможно, рекордер не сможет записать закрытие диалогового окна, в
    котором пользователь подтверждает удаление контакта. Это делается так:
    wd.switch_to_alert().accept()

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,7 +1,8 @@
+
 class ContactHelper:
 
     def __init__(self, app):
-        self.app=app
+        self.app = app
 
     def create(self, contact):
         wd = self.app.wd
@@ -81,6 +82,17 @@ class ContactHelper:
         wd.find_element_by_name("submit").click()
         self.return_home_page()
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # init contact creation
+        wd.find_element_by_link_text("home").click()
+        #select first contact
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+
     def return_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
+
+
