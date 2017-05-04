a7577e357df88d52899ac54007d843eabb8f3cf7
Lena <nikolskaya_elena@mail.ru>
2017-05-04 09:49:25 +0300

    не рабочая версия

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,6 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="malkons", middlename="don", lastname="perli", nickname="opr", title="dert",
                                company="esrt", address="street arbat", home="8394-33", mobile="4-4",
                                work="345-3", fax="345-213", email="test@gmaik.ty", email2="pes@kf.re", email3="pdt@ll.et",
@@ -15,11 +14,9 @@ def test_add_contact(app):
                                month2="//div[@id='content']/form/select[4]//option[3]", ayear="1930",
                                address2="street borovaya", phone2="44-532",
                                notes="primer"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="",
                                company="", address="", home="", mobile="",
                                work="", fax="", email="", email2="", email3="",
@@ -30,4 +27,3 @@ def test_add_empty_contact(app):
                                month2="//div[@id='content']/form/select[4]//option[1]", ayear="",
                                address2="", phone2="",
                                notes=""))
-    app.session.logout()
--- test/test_editing_contact.py
+++ test/test_editing_contact.py
@@ -1,4 +1,2 @@
 def test_editing_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.editing_first_contact()
-    app.session.logout()
