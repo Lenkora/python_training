f1366af48fa8268f01b9c5e4a4d0919cf6d9807e
Lena <nikolskaya_elena@mail.ru>
2017-05-04 11:27:43 +0300

    стабильная версия

--- test/test_editing_contact.py
+++ test/test_editing_contact.py
@@ -1,9 +1,2 @@
-from model.contact import Contact
-
-
 def test_editing_first_contact(app):
-    app.contact.editing_first_contact(Contact(firstname="NEW firstname"))
-
-
-#def test_editing_first_contact(app):
-    #app.contact.editing_first_contact(Contact(day1="//div[@id='content']/form/select[1]//option[10]"))
+    app.contact.editing_first_contact()
